20c43a630de91355b10c22001db42684a390f216
Aida <ai.abdirakhman@gmail.com>
2024-05-28 12:25:06 +0500

    Initial project setup with basic view controllers and CocoaPods

---
+++ Neobis_iOS_UIScreens/Custom/TabBarController/ScreensTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  ScreensTabBarController.swift
+//  Neobis_iOS_UIScreens
+//
+//  Created by Ai Hawok on 28/05/2024.
+//
+
+import UIKit
+
+class ScreensTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+   
+
+}
